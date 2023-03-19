824e808b3e06afe79e0127e17cec5d077f063dd3
Amir Gohari <night70@users.noreply.github.com>
2023-03-19 12:53:56 -0700

    the vistory function work good but i should separate some function independently

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ function Xfun(id) {
     if(tmp=="" && tmp!="O"){
         document.getElementById(id).innerHTML = "X";
         Ofun();
-        judge();
+        if(judge()){
+            clearTheTable();
+            alert("some body win!")
+        }
     }
-    
 }
 function Ofun(){
     let arr = document.getElementsByTagName("td");
@@ -19,10 +21,32 @@ function Ofun(){
     if(tdid){
         document.getElementById(tdid).innerHTML = "O";
     }
-
-    // console.log(tdid);
+    
+    console.log(judge());
 }
 function judge(){
+    let correctPattern = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,5,8],
+        [2,4,6],
+    ];
     let allCube = document.getElementsByTagName("td");
-    
+    // return allCube[0].innerText ;
+    for (const way of correctPattern) {
+        if(allCube[way[0]].innerText==allCube[way[1]].innerText && 
+            allCube[way[1]].innerText==allCube[way[2]].innerText &&
+            allCube[way[0]].innerText!="")
+            return true;
+    }
+}
+function clearTheTable(){
+    let cleaner = document.getElementsByTagName('td');
+    for (const iterator of cleaner) {
+        iterator.innerText = "";
+    }
 }
